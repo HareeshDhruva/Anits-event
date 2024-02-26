d4aacf9ce799d76f6b05b45c55b721c484c3f06c
hareeshdhruva <hareeshdhruva123@gmail.com>
2024-02-26 22:03:34 +0530

    solve

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -9,7 +9,7 @@ const page = () => {
         </div>
       <div className="flex justify-center items-center flex-col h-screen z-10">
         <h1 className="text-[80px] uppercase font-bold text-[#ff0099]">anits fest 2024</h1>
-        <p className="text-[20px] italic">Uniting Passion, Igniting Talent!</p>
+        <p className="text-[20px] italic">Uniting Passion, Igniting Talent!x</p>
       </div>
     </div>
   );
--- src/app/components/alternatives.jsx
+++ src/app/components/alternatives.jsx
@@ -4,14 +4,15 @@ const Alternatives = () => {
     <div className="my-20 mx-[50px] flex flex-col gap-[40px]">
       <div className="flex justify-center gap-[165px] items-center p-1">
         <div className="w-1/2">
-          <h1 className="my-[20px] font-bold text-[35px] text-[#ff0099]">INVENTUM_2K24</h1>
+          <h1 className="my-[20px] font-bold text-[35px] text-[#ff0099]">
+            INVENTUM_2K24
+          </h1>
           <p>
-            Attending a Holi festival feels like so much more than just throwing
-            colors and taking a few pictures. It gets you out of the monotony of
-            late winter and interacting with friends or family you might not
-            normally have fun with. The Holi celebrations I attended were always
-            extremely inclusive my Hindu relatives, young and old, were excited
-            to share the day with anyone interested in celebrating.
+            Dive into a world of creativity and innovation! Experience Memes
+            Mania, ML Hackathon, Poster Presentation, Live Quiz, Web Design,
+            Code Rush, Project Expo, Ad Mad Show, Reverse Coding, and more. Stay
+            tuned for updates as we redefine boundaries and celebrate ingenuity
+            together!
           </p>
         </div>
         <div>
@@ -19,32 +20,36 @@ const Alternatives = () => {
         </div>
       </div>
 
-      <div>
+      <div className="flex justify-center items-center p-1 gap-[165px]">
         <div>
           <img src="cursor.png" alt="Cursor logo" />
         </div>
         <div className="w-1/2">
-          <h1 className="my-[20px] font-bold text-[35px] text-[#ff0099]">CURSOR_2k24</h1>
+          <h1 className="my-[20px] font-bold text-[35px] text-[#ff0099]">
+            CURSOR_2k24
+          </h1>
           <p>
-            One of the most remarkable things about the Indian color festival is
-            how enthusiastic and care-free it is. It is purely a day of fun. I
-            didnt love being dragged to temple as a child, but going to temple
-            for Holi was an exception. Because running around throwing colors
-            was something I could always get on board with.
+            Delve into the digital realm with our CSE Departments extravaganza!
+            Experience the electrifying beats of Rhythm Rumble, immerse yourself
+            in Pixel Party, embark on a thrilling Treasure Hunt, navigate
+            through the Mind Maze, showcase your charm in Mr. Miss Cursor, and
+            unleash your creativity in Arts and Crafts. Join us for an
+            unforgettable journey blending technology and creativity!
           </p>
         </div>
       </div>
 
-      <div>
+      <div className="flex justify-center gap-[165px] items-center p-1">
         <div className="w-1/2">
-          <h1 className="my-[20px] font-bold text-[35px] text-[#ff0099]">IGNITION_2k24</h1>
+          <h1 className="my-[20px] font-bold text-[35px] text-[#ff0099]">
+            IGNITION_2k24
+          </h1>
           <p>
-            Attending a Holi festival feels like so much more than just throwing
-            colors and taking a few pictures. It gets you out of the monotony of
-            late winter and interacting with friends or family you might not
-            normally have fun with. The Holi celebrations I attended were always
-            extremely inclusive — my Hindu relatives, young and old, were excited
-            to share the day with anyone interested in celebrating.
+            Explore the pinnacle of innovation with our Mechanical Departments
+            student tech fest. Engage in mind-bending challenges including a
+            General Quiz, Technical Quiz, Paper Presentation, Startup Pitch,
+            Water Rocket competition, and more. Join us as we ignite curiosity
+            and push the boundaries of mechanical engineering prowess!
           </p>
         </div>
         <div>
@@ -57,13 +62,17 @@ const Alternatives = () => {
           <img src="elektra.png" alt="Elektra logo" />
         </div>
         <div className="w-1/2">
-          <h1 className="my-[20px] font-bold text-[35px] text-[#ff0099]">ELEKTRA_2K24</h1>
+          <h1 className="my-[20px] font-bold text-[35px] text-[#ff0099]">
+            ELEKTRA_2K24
+          </h1>
           <p>
-            One of the most remarkable things about the Indian color festival is
-            how enthusiastic and carefree it is. It is purely a day of fun. I
-            didnt love being dragged to temple as a child, but going to temple
-            for Holi was an exception. Because running around throwing colors
-            was something I could always get on board with.
+            Embark on an electrifying journey at Elektra 2K24! From cutting-edge
+            tech exhibitions to captivating non-tech events, our Department of
+            Electronics and Communication Engineering presents a fusion of
+            innovation and creativity. Join us for a spectacular showcase
+            featuring Technex, Code Craft, Verbal Odyssey, Robo Race Rush,
+            Utkarsh, Mega Mind, Circuit Carnival, LAN Fiesta, Capture a Minute,
+            Kaleidosave, and Kheel Mahotsav.
           </p>
         </div>
       </div>
