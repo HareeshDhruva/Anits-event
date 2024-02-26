0fa933b72a0de7947e03f02872ea6d8740d2a1e0
hareeshdhruva <hareeshdhruva123@gmail.com>
2024-02-26 23:35:14 +0530

    final commit

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -1,12 +1,16 @@
 "use client"
 import { motion} from "framer-motion";
+import Image from "next/image";
 const page = () => {
   return (
     <div>
       <div>
         <h1 className="text-center my-10 text-[50px] uppercase font-bold text-[#ff0099]">Gallery</h1>
       </div>
-      <div className="grid grid-cols-2 grid-rows-2 mx-[50px]">
+      <div className="z-0">
+          <Image src="bg.svg" alt="" className="opacity-[0.3] left-0 right-0 top-10 fixed z-0" width={1500} height={1200} />
+      </div>
+      <div className="grid grid-cols-2 grid-rows-2 mx-[50px] z-10">
         <motion.div className="col-span-1 row-span-2 flex justify-center items-center"
           initial={{ x:-400 }}
           transition={{ duration: 2 }}
@@ -29,7 +33,7 @@ const page = () => {
             <img src="coding.png" alt="" className="rounded-lg"/>
         </motion.div>
       </div>
-      <motion.div className="flex justify-center items-center my-12"
+      <motion.div className="flex justify-center items-center my-12 z-10"
        initial={{scale:0}}
        transition={{ duration:2,ease:"easeInOut"}}
        whileInView={{scale:1}}
@@ -37,7 +41,7 @@ const page = () => {
         <img src="reverse coding.png" alt="" className="rounded-lg"/>
       </motion.div>
       <div className="flex justify-evenly mx-[50px] my-12">
-      <motion.div className="flex justify-center items-center"
+      <motion.div className="flex justify-center items-center z-10"
        initial={{ x:300 }}
        transition={{ duration: 2 }}
        whileInView={{ x: 0 }}
